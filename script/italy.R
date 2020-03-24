e9ace8ddc795119afae5ccd65e77cfc381421dc1
AnthonyEbert <anthonyebert@gmail.com>
2020-03-24 19:04:19 +0100

    protezione civile 3091e876228970c42b91ad04ebfcc5dc57a566e0

--- script/italy.R
+++ script/italy.R
@@ -1,10 +1,16 @@
 
 italy <- all_countries %>%
-  filter(Country.Region == "Italy", Province.State == "total")
+  filter(Country.Region == "Italy", Province.State == "total") %>%
+  mutate(
+    susc_not_ill = 60317000 - confirmed
+  )
 
 readr::write_csv(italy, "data-raw/italy_all.csv")
 
 lombardy <- all_countries %>%
-  filter(Country.Region == "Italy", Province.State == "Lombardia")
+  filter(Country.Region == "Italy", Province.State == "Lombardia") %>%
+  mutate(
+    susc_not_ill = 10060574 - confirmed
+  )
 
 readr::write_csv(lombardy, "data-raw/lombardy_all.csv")
